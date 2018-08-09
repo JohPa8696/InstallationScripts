ba9cc33a280680ddd1ef8584c4b982c9676e9435
John <dpha010@aucklanduni.ac.nz>
2018-08-09 04:23:25 +0000

    sql and ssms installation scripts

---
+++ Install_SQL_for_me.ps1
@@ -0,0 +1,116 @@
+﻿# This powershell script automate some parts in the installation of SQL Server 2016
+# Name: install_sql_for_me
+# Date: 8/8/2018
+# Author: John Pham
+
+# Requires: $MinimumVersion of Powershell
+
+Param ([string]$sqlserver = ".",
+        [string]$SQLInstallerAbsolutePath = "C:\shares\en_sql_server_2017_standard_x64_dvd_11294407.iso",
+        [switch]$CreateMSA =$True,
+        [string]$ScriptPath = "C:\Users\Administrator\Desktop\SQLInstallationScripts\Config_Scripts"
+        )
+
+# [parameter(Mandatory=$True)]
+Set-StrictMode -Version 5
+Import-Module ServerManager;
+
+# Open firewall port
+netsh advfirewall firewall add rule name="SQL Instances" dir=in action=allow protocol=tcp localport=1433;
+
+
+
+function GetDotNetFramework()
+{
+    $WindowsVersion = [environment]::OSVersion.Version 
+    
+    If (($WindowsVersion.Major -eq 6) -And ($WindowsVersion.Minor -lt 2)) {
+    Add-WindowsFeature AS-Net-Framework;
+    }
+    ElseIf (($WindowsVersion.Major -eq 6) -And ($WindowsVersion.Minor -ge 2)) {
+        Add-WindowsFeature Net-Framework-Core;
+    }
+    ElseIf ($WindowsVersion.Major -eq 10){
+        Add-WindowsFeature Net-Framework-45-Core;
+    }
+}
+
+
+
+function CreateTempDirectory([string] $path)
+{
+    If(!(test-path $path))
+    {
+          New-Item -ItemType Directory -Force -Path $path
+    }
+}
+
+
+
+function CopyInstallers()
+{
+    $TempFolder = "C:\Temp"
+
+    CreateTempDirectory($TempFolder)
+
+    Copy-Item -Path $SQLInstallerAbsolutePath -Destination $TempFolder
+    $InstallerPath = $TempFolder+ "\" + (Split-Path $SQLInstallerAbsolutePath -leaf)
+    if (!(Test-Path $InstallerPath)){
+        Write-Host "Copy installer failed!"
+    } 
+    return $InstallerPath
+}
+
+
+
+function MountInstaller([string]$InstallerPath)
+{
+    $MountResult = Mount-DiskImage -ImagePath $InstallerPath -PassThru
+    $MountExePath = ($MountResult | Get-Volume).DriveLetter +":\setup.exe"
+
+    return $MountExePath
+}
+
+
+
+function DisMountInstaller([string]$InstallerPath)
+{
+   $MountResult = DisMount-DiskImage -ImagePath $InstallerPath
+}
+
+
+
+function InstallSQLServer([string] $SQLInstallerPath)
+{
+    $install = 'Start-Process -verb runas -FilePath "' + $SQLInstallerPath + '" -ArgumentList /ConfigurationFile="' + $ScriptPath + '\ConfigurationFile.ini" -Wait';
+
+    Invoke-Expression $Install;
+}
+
+
+# New-Item -ItemType directory -Path D:\SQL_Data
+# New-Item -ItemType directory -Path C:\SQL_Logs
+# New-Item -ItemType directory -Path C:\SQLAgentLogs
+# New-Item -ItemType directory -Path C:\Temp_DB
+
+# Copy installers from fileshares to temp folder
+
+$InstallerPath = CopyInstallers
+Write-Host $InstallerPath -verbose
+
+$MountExePath = MountInstaller($InstallerPath)
+Write-Host $MountExePath -verbose
+
+# Install dependencies
+GetDotNetFramework
+
+# Install
+InstallSQLServer ($MountExePath)
+
+# Dismount the installer
+
+DisMountInstaller($InstallerPath)
+
+# Clear
+
+Write-Host "SQL Server 2017 installation complete..." -ForegroundColor Green
---
+++ Install_SSMS_for_me.ps1
@@ -0,0 +1,42 @@
+﻿Param
+(
+    [string]$SSMSInstallerPath="C:\shares\SSMS-Setup-ENU.exe"
+)
+
+# [parameter(Mandatory=$True)]
+
+function CreateTempDirectory([string] $path)
+{
+    If(!(test-path $path))
+    {
+          New-Item -ItemType Directory -Force -Path $path
+    }
+}
+
+
+
+function CopyInstallers()
+{
+    $TempFolder = "C:\Temp"
+
+    CreateTempDirectory($TempFolder)
+
+    Copy-Item -Path $SSMSInstallerPath -Destination $TempFolder
+    $InstallerPath = $TempFolder+ "\" + (Split-Path $SSMSInstallerPath -leaf)
+    if (!(Test-Path $InstallerPath)){
+        Write-Host "Copy installer failed!"
+    } 
+    return $InstallerPath
+}
+
+
+
+$InstallerPath = CopyInstallers
+
+Write-Host "Beginning SSMS 2017 install..."
+
+$Params = " /Install /Quiet /Norestart /Logs log.txt"
+$Prms = $Params.Split(" ")
+& "$InstallerPath" $Prms | Out-Null
+
+Write-Host "SSMS 2017 installation complete..." -ForegroundColor Green
